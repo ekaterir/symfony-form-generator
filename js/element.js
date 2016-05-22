a17fb26e79df4fe0ccfc0c995f2b869623d55e9c
Ekaterina Ryabtseva <kate.ryabtseva@gmail.com>
2016-05-21 23:13:56 -0400

    Create getHTML method for element

--- js/element.js
+++ js/element.js
@@ -3,3 +3,47 @@ function Element(name) {
     this.name = name;
 
 }
+
+
+Element.prototype.getHTML = function() {
+
+    switch(this.name) {
+        case 'text':
+            return '<div class="form-group">' +
+  		   '<label for="usr">Name:</label>' +
+  		   '<input type="text" class="form-control" id="usr">' +
+		   '</div>';
+            break;
+        case 'checkbox':
+            return '<div class="checkbox">' +
+		   '<label>' +
+      		   '<input type="checkbox"> Check me out' +
+    		   '</label>' +
+  		   '</div>';
+            break;
+        case 'radio':
+            return '<div class="radio">' +
+  		   '<label><input type="radio" name="optradio">Option 1</label>' +
+		   '</div>';
+            break;
+        case 'email':
+            return '<div class="form-group">' +
+    		   '<label for="email">Email address:</label>' +
+    		   '<input type="email" class="form-control" id="email">' +
+  		   '</div>';
+            break;
+        case 'password':
+            return '<div class="form-group">' +
+    		   '<label for="pwd">Password:</label>' +
+    		   '<input type="password" class="form-control" id="pwd">' +
+  		   '</div>';
+            break;
+        case 'textarea':
+            return '<div class="form-group">' +
+  		   '<label for="comment">Comment:</label>' +
+  		   '<textarea class="form-control" rows="5" id="comment"></textarea>' +
+		   '</div>';
+            break;
+	}
+
+}
--- js/form-builder.js
+++ js/form-builder.js
@@ -1,7 +1,8 @@
   $(function() {
+
     var icons = {
-      header: "fa fa-fw fa-plus-circle txt-color-green",
-      activeHeader: "fa fa-fw fa-minus-circle txt-color-red"
+      header: "fa fa-fw fa-chevron-circle-right txt-color-green",
+      activeHeader: "fa fa-fw fa-chevron-circle-down txt-color-red"
     };
 
     $( "#form-types" ).accordion({
@@ -20,24 +21,19 @@
       hoverClass: "ui-state-hover",
       accept: ":not(.ui-sortable-helper)",
       drop: function( event, ui ) {
-        $( this ).find( ".placeholder" ).remove();
-	printHtmlOf(ui.draggable.text().toLowerCase());
-	var element = new Element(ui.draggable.text().toLowerCase());
+        $(this).find( ".placeholder" ).remove();
 	if (typeof window.form === 'undefined') {
 		var form = new Form('rand string');
-		form.addElement(element);
 		window.form = form;
-		console.log('made a new form');
-	} else {
-		window.form.addElement(element);
-		console.log(window.form);
-	}	
+	}
+	var element = new Element(ui.draggable.text().toLowerCase());
+	window.form.addElement(element);
+	printHtmlOf(element.getHTML());
+	console.log(window.form);
       }
     }).sortable({
       items: "div",
       sort: function() {
-        // gets added unintentionally by droppable interacting with sortable
-        // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
         $( this ).removeClass( "ui-state-default" );
       }
     });
@@ -47,47 +43,10 @@
 
 
 
-  var printHtmlOf = function (text) {
+  var printHtmlOf = function (html) {
 	var newDiv = document.createElement('div');
 	newDiv.className = 'form-group';
-	switch(text) {
-		case 'text':
-			newDiv.innerHTML = '<div class="form-group">' +
-  				'<label for="usr">Name:</label>' +
-  				'<input type="text" class="form-control" id="usr">' +
-				'</div>';
-			break;
-		case 'checkbox':
-			newDiv.innerHTML = '<div class="checkbox">' +
-				'<label>' +
-      				'<input type="checkbox"> Check me out' +
-    				'</label>' +
-  				'</div>';
-			break;
-		case 'radio':
-			newDiv.innerHTML = '<div class="radio">' +
-  				'<label><input type="radio" name="optradio">Option 1</label>' +
-				'</div>';
-			break;
-		case 'email':
-			newDiv.innerHTML = '<div class="form-group">' +
-    				'<label for="email">Email address:</label>' +
-    				'<input type="email" class="form-control" id="email">' +
-  				'</div>';
-			break;
-		case 'password':
-			newDiv.innerHTML = '<div class="form-group">' +
-    				'<label for="pwd">Password:</label>' +
-    				'<input type="password" class="form-control" id="pwd">' +
-  				'</div>';
-			break;
-		case 'textarea':
-			newDiv.innerHTML = '<div class="form-group">' +
-  				'<label for="comment">Comment:</label>' +
-  				'<textarea class="form-control" rows="5" id="comment"></textarea>' +
-				'</div>';
-			break;
-	}
+        newDiv.innerHTML = html;
 	var element = document.getElementById("form-preview");
 	element.appendChild(newDiv);
   };
